ff66f7a8f11a732d60a458b92a3f8bf36cdf1447
dyiaxo <md.ali@razorpay.com>
2026-01-10 02:45:41 +0530

    feat: implemented regex search(demo) for orders.create

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,81 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Project, SyntaxKind, Node } from 'ts-morph';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const project = new Project({
+    useInMemoryFileSystem: true
+});
+
 export function activate(context: vscode.ExtensionContext) {
+	const diagnostic = vscode.languages.createDiagnosticCollection('razorpay-linter');
+    
+    context.subscriptions.push(diagnostic);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rzp-ext" is now active!');
+    vscode.workspace.onDidOpenTextDocument((document) => {
+        lintDocument(document, diagnostic);
+    });
+    
+    vscode.workspace.onDidChangeTextDocument((event) => {
+        lintDocument(event.document, diagnostic);
+    });
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('rzp-ext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from rzp-ext!');
-	});
+export function lintDocument(
+  document: vscode.TextDocument,
+  diagnosticCollection: vscode.DiagnosticCollection
+) {
+  if (!["javascript", "typescript"].includes(document.languageId)) return;
 
-	context.subscriptions.push(disposable);
+  const diagnostics: vscode.Diagnostic[] = [];
+  const sourceFile = project.createSourceFile(
+    document.fileName,
+    document.getText(),
+    { overwrite: true }
+  );
+
+  sourceFile.forEachDescendant(node => {
+    if (!Node.isCallExpression(node)) return;
+
+    const expr = node.getExpression();
+    if (!Node.isPropertyAccessExpression(expr)) return;
+
+    // expr === orders.create
+    if (expr.getName() !== "create") return;
+
+    const left = expr.getExpression();
+    if (!Node.isPropertyAccessExpression(left)) return;
+
+    if (left.getName() !== "orders") return;
+
+    const args = node.getArguments();
+    if (args.length === 0) return;
+
+    const firstArg = args[0];
+    if (!Node.isObjectLiteralExpression(firstArg)) return;
+
+    const passedParams = firstArg
+      .getProperties()
+      .filter(Node.isPropertyAssignment)
+      .map(p => p.getName());
+
+    ["amount", "currency"].forEach(param => {
+      if (!passedParams.includes(param)) {
+        const range = new vscode.Range(
+          document.positionAt(node.getStart()),
+          document.positionAt(node.getEnd())
+        );
+
+        diagnostics.push(
+          new vscode.Diagnostic(
+            range,
+            `Missing required parameter "${param}" in orders.create`,
+            vscode.DiagnosticSeverity.Warning
+          )
+        );
+      }
+    });
+  });
+
+  diagnosticCollection.set(document.uri, diagnostics);
 }
 
 // This method is called when your extension is deactivated
